7efe8c9de4615395348ae6d3aad6392628cf8b8f
mkuegeler <83090020+5deen@users.noreply.github.com>
2023-02-09 19:29:16 +0100

    imagesets updated

--- build/release.js
+++ build/release.js
@@ -20,51 +20,84 @@ async function instantiate(module, imports = {}) {
     initString(init_set) {
       // assembly/index/initString(~lib/string/String) => ~lib/array/Array<~lib/array/Array<~lib/string/String>>
       init_set = __lowerString(init_set) || __notnull();
-      return __liftArray(pointer => __liftArray(pointer => __liftString(new Uint32Array(memory.buffer)[pointer >>> 2]), 2, new Uint32Array(memory.buffer)[pointer >>> 2]), 2, exports.initString(init_set) >>> 0);
+      return __liftArray(pointer => __liftArray(pointer => __liftString(__getU32(pointer)), 2, __getU32(pointer)), 2, exports.initString(init_set) >>> 0);
     },
     initNumbers(init_set) {
       // assembly/index/initNumbers(~lib/array/Array<i32>) => ~lib/array/Array<~lib/array/Array<i32>>
-      init_set = __lowerArray((pointer, value) => { new Int32Array(memory.buffer)[pointer >>> 2] = value; }, 8, 2, init_set) || __notnull();
-      return __liftArray(pointer => __liftArray(pointer => new Int32Array(memory.buffer)[pointer >>> 2], 2, new Uint32Array(memory.buffer)[pointer >>> 2]), 2, exports.initNumbers(init_set) >>> 0);
+      init_set = __lowerArray(__setU32, 10, 2, init_set) || __notnull();
+      return __liftArray(pointer => __liftArray(__getI32, 2, __getU32(pointer)), 2, exports.initNumbers(init_set) >>> 0);
     },
-    createMarkupElement(name, template) {
-      // assembly/index/createMarkupElement(~lib/string/String, ~lib/string/String) => ~lib/string/String
-      name = __retain(__lowerString(name) || __notnull());
-      template = __lowerString(template) || __notnull();
+    SVGNode(w, h, children) {
+      // assembly/index/SVGNode(~lib/string/String, ~lib/string/String, ~lib/array/Array<~lib/string/String>) => ~lib/string/String
+      w = __retain(__lowerString(w) || __notnull());
+      h = __retain(__lowerString(h) || __notnull());
+      children = __lowerArray((pointer, value) => { __setU32(pointer, __lowerString(value) || __notnull()); }, 6, 2, children) || __notnull();
       try {
-        return __liftString(exports.createMarkupElement(name, template) >>> 0);
+        return __liftString(exports.SVGNode(w, h, children) >>> 0);
       } finally {
-        __release(name);
+        __release(w);
+        __release(h);
       }
     },
-    setPanels(panels) {
-      // assembly/index/setPanels(i32) => ~lib/string/String
-      return __liftString(exports.setPanels(panels) >>> 0);
+    StyleNode(children) {
+      // assembly/index/StyleNode(~lib/array/Array<~lib/string/String>) => ~lib/string/String
+      children = __lowerArray((pointer, value) => { __setU32(pointer, __lowerString(value) || __notnull()); }, 6, 2, children) || __notnull();
+      return __liftString(exports.StyleNode(children) >>> 0);
     },
-    createSVGPattern(init_set, template) {
-      // assembly/index/createSVGPattern(~lib/string/String, ~lib/string/String) => ~lib/string/String
-      init_set = __retain(__lowerString(init_set) || __notnull());
-      template = __lowerString(template) || __notnull();
+    GroupNode(id, children, transform) {
+      // assembly/index/GroupNode(~lib/string/String, ~lib/array/Array<~lib/string/String>, ~lib/string/String?) => ~lib/string/String
+      id = __retain(__lowerString(id) || __notnull());
+      children = __retain(__lowerArray((pointer, value) => { __setU32(pointer, __lowerString(value) || __notnull()); }, 6, 2, children) || __notnull());
+      transform = __lowerString(transform) || __notnull();
       try {
-        return __liftString(exports.createSVGPattern(init_set, template) >>> 0);
+        exports.__setArgumentsLength(arguments.length);
+        return __liftString(exports.GroupNode(id, children, transform) >>> 0);
       } finally {
-        __release(init_set);
+        __release(id);
+        __release(children);
       }
     },
-    createImagePattern(svg, img) {
-      // assembly/index/createImagePattern(~lib/string/String, ~lib/string/String) => ~lib/string/String
-      svg = __retain(__lowerString(svg) || __notnull());
-      img = __lowerString(img) || __notnull();
+    RectNode(id, x, y, w, h, cl, transform) {
+      // assembly/index/RectNode(~lib/string/String, ~lib/string/String, ~lib/string/String, ~lib/string/String, ~lib/string/String, ~lib/string/String, ~lib/string/String?) => ~lib/string/String
+      id = __retain(__lowerString(id) || __notnull());
+      x = __retain(__lowerString(x) || __notnull());
+      y = __retain(__lowerString(y) || __notnull());
+      w = __retain(__lowerString(w) || __notnull());
+      h = __retain(__lowerString(h) || __notnull());
+      cl = __retain(__lowerString(cl) || __notnull());
+      transform = __lowerString(transform) || __notnull();
       try {
-        return __liftString(exports.createImagePattern(svg, img) >>> 0);
+        exports.__setArgumentsLength(arguments.length);
+        return __liftString(exports.RectNode(id, x, y, w, h, cl, transform) >>> 0);
       } finally {
-        __release(svg);
+        __release(id);
+        __release(x);
+        __release(y);
+        __release(w);
+        __release(h);
+        __release(cl);
       }
     },
-    createAbstractRectangle(id) {
-      // assembly/index/createAbstractRectangle(~lib/string/String) => ~lib/array/Array<assembly/geometry/Point>
-      id = __lowerString(id) || __notnull();
-      return __liftArray(pointer => __liftInternref(new Uint32Array(memory.buffer)[pointer >>> 2]), 2, exports.createAbstractRectangle(id) >>> 0);
+    LineNode(id, x1, y1, x2, y2, cl, transform) {
+      // assembly/index/LineNode(~lib/string/String, ~lib/string/String, ~lib/string/String, ~lib/string/String, ~lib/string/String, ~lib/string/String, ~lib/string/String?) => ~lib/string/String
+      id = __retain(__lowerString(id) || __notnull());
+      x1 = __retain(__lowerString(x1) || __notnull());
+      y1 = __retain(__lowerString(y1) || __notnull());
+      x2 = __retain(__lowerString(x2) || __notnull());
+      y2 = __retain(__lowerString(y2) || __notnull());
+      cl = __retain(__lowerString(cl) || __notnull());
+      transform = __lowerString(transform) || __notnull();
+      try {
+        exports.__setArgumentsLength(arguments.length);
+        return __liftString(exports.LineNode(id, x1, y1, x2, y2, cl, transform) >>> 0);
+      } finally {
+        __release(id);
+        __release(x1);
+        __release(y1);
+        __release(x2);
+        __release(y2);
+        __release(cl);
+      }
     },
     createSVGDocument(ratio, map, text) {
       // assembly/index/createSVGDocument(f64, ~lib/string/String, ~lib/string/String) => ~lib/string/String
@@ -100,9 +133,8 @@ async function instantiate(module, imports = {}) {
   function __liftArray(liftElement, align, pointer) {
     if (!pointer) return null;
     const
-      memoryU32 = new Uint32Array(memory.buffer),
-      dataStart = memoryU32[pointer + 4 >>> 2],
-      length = memoryU32[pointer + 12 >>> 2],
+      dataStart = __getU32(pointer + 4),
+      length = __dataview.getUint32(pointer + 12, true),
       values = new Array(length);
     for (let i = 0; i < length; ++i) values[i] = liftElement(dataStart + (i << align >>> 0));
     return values;
@@ -112,24 +144,15 @@ async function instantiate(module, imports = {}) {
     const
       length = values.length,
       buffer = exports.__pin(exports.__new(length << align, 1)) >>> 0,
-      header = exports.__pin(exports.__new(16, id)) >>> 0,
-      memoryU32 = new Uint32Array(memory.buffer);
-    memoryU32[header + 0 >>> 2] = buffer;
-    memoryU32[header + 4 >>> 2] = buffer;
-    memoryU32[header + 8 >>> 2] = length << align;
-    memoryU32[header + 12 >>> 2] = length;
+      header = exports.__pin(exports.__new(16, id)) >>> 0;
+    __setU32(header + 0, buffer);
+    __dataview.setUint32(header + 4, buffer, true);
+    __dataview.setUint32(header + 8, length << align, true);
+    __dataview.setUint32(header + 12, length, true);
     for (let i = 0; i < length; ++i) lowerElement(buffer + (i << align >>> 0), values[i]);
     exports.__unpin(buffer);
     exports.__unpin(header);
     return header;
-  }
-  class Internref extends Number {}
-  const registry = new FinalizationRegistry(__release);
-  function __liftInternref(pointer) {
-    if (!pointer) return null;
-    const sentinel = new Internref(__retain(pointer));
-    registry.register(sentinel, pointer);
-    return sentinel;
   }
   const refcounts = new Map();
   function __retain(pointer) {
@@ -151,17 +174,42 @@ async function instantiate(module, imports = {}) {
   function __notnull() {
     throw TypeError("value must not be null");
   }
+  let __dataview = new DataView(memory.buffer);
+  function __setU32(pointer, value) {
+    try {
+      __dataview.setUint32(pointer, value, true);
+    } catch {
+      __dataview = new DataView(memory.buffer);
+      __dataview.setUint32(pointer, value, true);
+    }
+  }
+  function __getI32(pointer) {
+    try {
+      return __dataview.getInt32(pointer, true);
+    } catch {
+      __dataview = new DataView(memory.buffer);
+      return __dataview.getInt32(pointer, true);
+    }
+  }
+  function __getU32(pointer) {
+    try {
+      return __dataview.getUint32(pointer, true);
+    } catch {
+      __dataview = new DataView(memory.buffer);
+      return __dataview.getUint32(pointer, true);
+    }
+  }
   return adaptedExports;
 }
 export const {
   memory,
   initString,
   initNumbers,
-  createMarkupElement,
-  setPanels,
-  createSVGPattern,
-  createImagePattern,
-  createAbstractRectangle,
+  SVGNode,
+  StyleNode,
+  GroupNode,
+  RectNode,
+  LineNode,
   createSVGDocument
 } = await (async url => instantiate(
   await (async () => {
