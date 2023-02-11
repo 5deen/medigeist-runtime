15dd69e0dc37ad61a9a425fa1899f9e4af815ab0
Design Machine <83090020+5deen@users.noreply.github.com>
2023-02-11 18:51:03 +0000

    Add colors.js

---
+++ colors.js
@@ -0,0 +1,44 @@
+// References
+
+// https://imagecolorpicker.com
+// https://www.random.org/colors/hex
+
+let webcolors = [
+  {
+    "div": 2,
+    "line_color": "#ff0000",
+    "canvas_color": "#cc00cc"
+  },
+  {
+    "div": 5,
+    "line_color": "#00cc88",
+    "canvas_color": "#008080"
+  },
+  {
+    "div": 8,
+    "line_color": "#de700f",
+    "canvas_color": "#461305"
+  }
+
+];
+
+let abstractbiotop = [
+  {
+    "div": 2,
+    "line_color": "#4c0903",
+    "canvas_color": "#ab9a75"
+  },
+  {
+    "div": 5,
+    "line_color": "#ab9a75",
+    "canvas_color": "#4c0903"
+  },
+  {
+    "div": 8,
+    "line_color": "#de700f",
+    "canvas_color": "#461305"
+  }
+
+];
+
+let colorset = [webcolors, abstractbiotop];
