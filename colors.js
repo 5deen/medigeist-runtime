394eacda2e584898478d2d07153f3de59ff7c686
Design Machine <83090020+5deen@users.noreply.github.com>
2023-02-12 10:23:29 +0000

    Update colors.js

--- colors.js
+++ colors.js
@@ -3,6 +3,109 @@
 // https://imagecolorpicker.com
 // https://www.random.org/colors/hex
 
+/*
+Farbraum: 32 Farben. 16 Kombinationen
+Die Primzahlen bis 100 lauten: 
+2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97
+
+// 0
+{"div": 2,
+"line_color":"#986ba1",
+"canvas_color":"#deb07a"
+},
+
+// 1
+{"div": 3,
+"line_color":"#c21706",
+"canvas_color":"#ab431a"
+},
+
+// 2
+{"div": 5,
+"line_color":"#deb07a",
+"canvas_color":"#633c54"
+},
+
+// 3
+{"div": 7,
+"line_color":"#516443",
+"canvas_color":"#ab9a75"
+},
+
+// 4
+{"div": 11,
+"line_color":"#516443",
+"canvas_color":"#be8b15"
+},
+
+// 5
+{"div": 13,
+"line_color":"#516443",
+"canvas_color":"#72714f"
+},
+
+// 6
+{"div": 17,
+"line_color":"#be8b15",
+"canvas_color":"#621d0d"
+},
+
+// 7
+{"div": 19,
+"line_color":"#c92d13",
+"canvas_color":"#7b2e15"
+},
+
+// 8
+{"div": 23,
+"line_color":"#516443",
+"canvas_color":"#210f09"
+},
+
+// 9
+{"div": 29,
+"line_color":"#ac6e11",
+"canvas_color":"#be8b15"
+},
+
+// 10
+{"div": 31,
+"line_color":"#986ba1",
+"canvas_color":"#633c54"
+},
+
+// 11
+{"div": 37,
+"line_color":"#db8536",
+"canvas_color":"#c92d13"
+},
+
+// 12
+{"div": 41,
+"line_color":"#ebb137",
+"canvas_color":"#5a110f"
+},
+
+// 13
+{"div":43,
+"line_color":"#72714f",
+"canvas_color":"#5a110f"
+},
+
+// 14
+{"div": 47,
+"line_color":"#516443",
+"canvas_color":"#b5a887"
+},
+
+// 15
+{"div": 53,
+"line_color":"#ebb137",
+"canvas_color: "#5e4716"
+}
+
+*/
+
 let webcolors = [
   {
     "div": 2,
@@ -23,21 +126,101 @@ let webcolors = [
 ];
 
 let abstractbiotop = [
-  {
-    "div": 2,
-    "line_color": "#4c0903",
-    "canvas_color": "#ab9a75"
-  },
-  {
-    "div": 5,
-    "line_color": "#ab9a75",
-    "canvas_color": "#4c0903"
-  },
-  {
-    "div": 8,
-    "line_color": "#de700f",
-    "canvas_color": "#461305"
-  }
+ // 0
+{"div": 2,
+"line_color":"#986ba1",
+"canvas_color":"#deb07a"
+},
+
+// 1
+{"div": 3,
+"line_color":"#c21706",
+"canvas_color":"#ab431a"
+},
+
+// 2
+{"div": 5,
+"line_color":"#deb07a",
+"canvas_color":"#633c54"
+},
+
+// 3
+{"div": 7,
+"line_color":"#516443",
+"canvas_color":"#ab9a75"
+},
+
+// 4
+{"div": 11,
+"line_color":"#516443",
+"canvas_color":"#be8b15"
+},
+
+// 5
+{"div": 13,
+"line_color":"#516443",
+"canvas_color":"#72714f"
+},
+
+// 6
+{"div": 17,
+"line_color":"#be8b15",
+"canvas_color":"#621d0d"
+},
+
+// 7
+{"div": 19,
+"line_color":"#c92d13",
+"canvas_color":"#7b2e15"
+},
+
+// 8
+{"div": 23,
+"line_color":"#516443",
+"canvas_color":"#210f09"
+},
+
+// 9
+{"div": 29,
+"line_color":"#ac6e11",
+"canvas_color":"#be8b15"
+},
+
+// 10
+{"div": 31,
+"line_color":"#986ba1",
+"canvas_color":"#633c54"
+},
+
+// 11
+{"div": 37,
+"line_color":"#db8536",
+"canvas_color":"#c92d13"
+},
+
+// 12
+{"div": 41,
+"line_color":"#ebb137",
+"canvas_color":"#5a110f"
+},
+
+// 13
+{"div":43,
+"line_color":"#72714f",
+"canvas_color":"#5a110f"
+},
+
+// 14
+{"div": 47,
+"line_color":"#516443",
+"canvas_color":"#b5a887"
+},
+
+// 15
+{"div": 53,
+"line_color":"#ebb137",
+"canvas_color": "#5e4716"
+}
 
 ];
 
